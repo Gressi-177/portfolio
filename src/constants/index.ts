824741856a87351064e55612fef682e82712b73a
Gressi-177 <vietdoan177@gmail.com>
2024-10-29 21:55:17 +0700

    Refactor: Update import paths for social icons in navbar component

---
+++ src/constants/index.ts
@@ -0,0 +1,24 @@
+import { SiFacebook, SiGithub, SiLinkedin, SiX } from 'react-icons/si'
+
+export const SOCIALS = [
+  {
+    lable: 'Facebook',
+    url: 'facebook.com/gressi.177',
+    icon: SiFacebook,
+  },
+  {
+    lable: 'Twitter',
+    url: 'twitter.com/gressi_177',
+    icon: SiX,
+  },
+  {
+    lable: 'GitHub',
+    url: 'github.com/gressi-177',
+    icon: SiGithub,
+  },
+  {
+    lable: 'LinkedIn',
+    url: 'linkedin.com/in/gressi-177',
+    icon: SiLinkedin,
+  },
+]
